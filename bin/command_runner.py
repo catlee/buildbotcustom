326b6832241d160edbe6bbcbac2095cbc31ffd4b
Chris AtLee <catlee@mozilla.com>
2011-06-16 18:24:09 -0700

    added nagios check made things more reliable added logging, and dead items

--- bin/command_runner.py
+++ bin/command_runner.py
@@ -10,43 +10,94 @@ import logging
 log = logging.getLogger(__name__)
 
 class CommandRunner(object):
+    # TODO: Configure these
+    max_retries = 5
+    retry_time = 60
+
     def __init__(self, queuedir, concurrency=1):
-        self.queuedir = QueueDir('commands', queuedir)
+        self.queuedir = queuedir
+        self.q = QueueDir('commands', queuedir)
         self.concurrency = concurrency
         self.active = []
 
+        # List of (requeue_time, item_id)
+        self.to_requeue = []
+
     def run(self, cmd, item_id):
+        if self.q.getcount(item_id) > self.max_retries:
+            log.info("Giving up on %s", item_id)
+            self.q.murder(item_id)
+            return
+
         log.info("Running %s", cmd)
-        # TODO: Where to stdout/stderr go?
+        output = self.q.getlog(item_id)
+        output.write("Running %s\n" % cmd)
+        output.flush()
         devnull = open(os.devnull, 'r')
-        p = subprocess.Popen(cmd, close_fds=True, stdin=devnull)
-        self.active.append((p, item_id))
+        try:
+            p = subprocess.Popen(cmd, close_fds=True, stdin=devnull, stdout=output, stderr=output)
+            self.active.append((p, item_id, output))
+        except OSError:
+            output.write("\nFailed with OSError; requeuing in %i seconds\n", self.retry_time)
+            # Wait to requeue it
+            # If we die, then it's still in cur, and will be moved back into 'new' eventually
+            self.to_requeue.append( (time.time() + self.retry_time, item_id) )
+
+    def requeue(self):
+        now = time.time()
+        for t, item_id in self.to_requeue:
+            if now > t:
+                log.info("Requeuing %s", item_id)
+                self.q.requeue(item_id)
+                self.to_requeue.remove( (t, item_id) )
+            else:
+                self.q.touch(item_id)
 
     def monitor(self):
-        for p, item_id in self.active[:]:
-            if p.poll() is not None:
-                self.queuedir.remove(item_id)
-                self.active.remove((p, item_id))
+        # TODO: Impose a maximum time on jobs
+        for p, item_id, output in self.active[:]:
+            self.q.touch(item_id)
+            result = p.poll()
+            if result is not None:
+                output.write("\nResult: %s\n" % result)
+                output.close()
+                self.active.remove((p, item_id, output))
+                if result == 0:
+                    self.q.remove(item_id)
+                else:
+                    log.warn("%s failed; requeuing", item_id)
+                    # Requeue it!
+                    self.q.requeue(item_id)
 
     def loop(self):
         """
         Main processing loop. Read new items from the queue and run them!
         """
         while True:
+            self.requeue()
             self.monitor()
             if len(self.active) >= self.concurrency:
                 # Wait!
                 time.sleep(1)
                 continue
 
-            item = self.queuedir.pop()
-            if not item:
-                self.queuedir.wait(1000)
-                continue
+            while len(self.active) < self.concurrency:
+                item = self.q.pop()
+                if not item:
+                    self.q.wait(1000)
+                    break
 
-            item_id, fp = item
-            command = json.load(fp)
-            self.run(command, item_id)
+                item_id, fp = item
+                try:
+                    command = json.load(fp)
+                    self.run(command, item_id)
+                except ValueError:
+                    # Couldn't parse it as json
+                    # There's no hope!
+                    self.q.log(item_id, "Couldn't load json; murdering")
+                    self.q.murder(item_id)
+                finally:
+                    fp.close()
 
 def main():
     from optparse import OptionParser
@@ -59,6 +110,7 @@ def main():
     parser.add_option("-q", "--queuedir", dest="queuedir")
     parser.add_option("-j", "--jobs", dest="concurrency", type="int")
 
+    # TODO: Log to another file? Use RotatingFileHandler?
     logging.basicConfig(level=logging.INFO)
 
     options, args = parser.parse_args()
--- status/queue.py
+++ status/queue.py
@@ -24,8 +24,16 @@ class _MovedHandler(pyinotify.ProcessEvent):
         pass
 
 class QueueDir(object):
+    # How long before things are considered to be "old"
+    # Also how long between cleanup jobs
     cleanup_time = 300 # 5 minutes
+
+    # Should the producer do cleanup?
+    producer_cleanup = True
+
+    # Mapping of names to QueueDir instances
     _objects = {}
+
     def __init__(self, name, queue_dir):
         self._objects[name] = self
         self.queue_dir = queue_dir
@@ -33,10 +41,13 @@ class QueueDir(object):
         self.pid = os.getpid()
         self.started = int(time.time())
         self.count = 0
+        self.last_cleanup = 0
 
         self.tmp_dir = os.path.join(self.queue_dir, 'tmp')
         self.new_dir = os.path.join(self.queue_dir, 'new')
         self.cur_dir = os.path.join(self.queue_dir, 'cur')
+        self.log_dir = os.path.join(self.queue_dir, 'logs')
+        self.dead_dir = os.path.join(self.queue_dir, 'dead')
 
         self.setup()
 
@@ -45,7 +56,7 @@ class QueueDir(object):
         return cls._objects[name]
 
     def setup(self):
-        for d in (self.tmp_dir, self.new_dir, self.cur_dir):
+        for d in (self.tmp_dir, self.new_dir, self.cur_dir, self.log_dir, self.dead_dir):
             if not os.path.exists(d):
                 os.makedirs(d, 0700)
             else:
@@ -56,18 +67,24 @@ class QueueDir(object):
     def cleanup(self):
         """
         Removes old items from tmp
+        Removes old logs from log_dir
         Moves old items from cur into new
 
         'old' is defined by the cleanup_time property
         """
         now = time.time()
-        for f in os.listdir(self.tmp_dir):
-            fn = os.path.join(self.tmp_dir, f)
-            try:
-                if os.path.getmtime(fn) < now - self.cleanup_time:
-                    os.unlink(fn)
-            except OSError:
-                pass
+        if now - self.last_cleanup < self.cleanup_time:
+            return
+        self.last_cleanup = now
+        dirs = [self.tmp_dir, self.log_dir]
+        for d in dirs:
+            for f in os.listdir(d):
+                fn = os.path.join(d, f)
+                try:
+                    if os.path.getmtime(fn) < now - self.cleanup_time:
+                        os.unlink(fn)
+                except OSError:
+                    pass
 
         for f in os.listdir(self.cur_dir):
             fn = os.path.join(self.cur_dir, f)
@@ -84,9 +101,8 @@ class QueueDir(object):
         """
         Adds a new item to the queue
         """
-        self.cleanup()
         # write data to tmp
-        fd, tmp_name = tempfile.mkstemp(prefix="%i-%i-%i" % (self.started, self.count, self.pid),
+        fd, tmp_name = tempfile.mkstemp(prefix="%i-%i-%i.0" % (self.started, self.count, self.pid),
                 dir=self.tmp_dir)
         os.write(fd, data)
         os.close(fd)
@@ -94,6 +110,9 @@ class QueueDir(object):
         dst_name = os.path.join(self.new_dir, os.path.basename(tmp_name))
         os.rename(tmp_name, dst_name)
         self.count += 1
+
+        if self.producer_cleanup:
+            self.cleanup()
 
     ###
     # For consumers
@@ -108,7 +127,7 @@ class QueueDir(object):
         self.cleanup()
         items = os.listdir(self.new_dir)
         if sorted:
-            items.sort(key=_intkeys)
+            items.sort(key=lambda f: os.path.getmtime(os.path.join(self.new_dir, f)))
         for item in items:
             try:
                 dst_name = os.path.join(self.cur_dir, item)
@@ -119,6 +138,37 @@ class QueueDir(object):
                 pass
         return None
 
+    def touch(self, item_id):
+        """
+        Indicate that we're still working on this item
+        """
+        fn = os.path.join(self.cur_dir, item_id)
+        os.utime(fn, None)
+
+    def getcount(self, item_id):
+        """
+        Returns how many times this item has been run
+        """
+        try:
+            return int(item_id.split(".")[1])
+        except:
+            return 0
+
+    def getlogname(self, item_id):
+        if "." in item_id:
+            item_id = item_id.split(".")[0]
+        fn = os.path.join(self.log_dir, "%s.log" % item_id)
+        return fn
+
+    def getlog(self, item_id):
+        """
+        Creates and returns a file object for a log file for this item
+        """
+        return open(self.getlogname(item_id), "a+")
+
+    def log(self, item_id, msg):
+        self.getlog(item_id).write(msg)
+
     def remove(self, item_id):
         """
         Removes item_id from cur
@@ -127,25 +177,40 @@ class QueueDir(object):
 
     def requeue(self, item_id):
         """
-        Moves item_id from cur back into new
+        Moves item_id from cur back into new, incrementing the counter at the
+        end
         """
-        dst_name = os.path.join(self.new_dir, item_id)
+        try:
+            core_item_id, count = item_id.split(".")
+            count = int(count)+1
+        except:
+            core_item_id = item_id
+            count = 1
+        dst_name = os.path.join(self.new_dir, "%s.%i" % (core_item_id, count))
         os.rename(os.path.join(self.cur_dir, item_id), dst_name)
         os.utime(dst_name, None)
 
-    def wait(self, timeout=None):
+    def murder(self, item_id):
         """
-        Waits for new items to arrive in new, call cb when we have something
+        Moves item_id and log from cur into dead for future inspection
         """
-        assert pyinotify
-        wm = pyinotify.WatchManager()
-        try:
-            wm.add_watch(self.new_dir, pyinotify.IN_MOVED_TO)
+        dst_name = os.path.join(self.dead_dir, item_id)
+        os.rename(os.path.join(self.cur_dir, item_id), dst_name)
+        if os.path.exists(self.getlogname(item_id)):
+            dst_name = os.path.join(self.dead_dir, "%s.log" % item_id)
+            os.rename(self.getlogname(item_id), dst_name)
 
-            notifier = pyinotify.Notifier(wm, _MovedHandler())
-            notifier.check_events(timeout)
-            # TODO: need to call these?
-            #notifier.read_events()
-            notifier.process_events()
-        finally:
-            wm.close()
+    if pyinotify:
+        def wait(self, timeout=None):
+            """
+            Waits for new items to arrive in new, call cb when we have something
+            """
+            wm = pyinotify.WatchManager()
+            try:
+                wm.add_watch(self.new_dir, pyinotify.IN_MOVED_TO)
+
+                notifier = pyinotify.Notifier(wm, _MovedHandler())
+                notifier.check_events(timeout)
+                notifier.process_events()
+            finally:
+                wm.close()
